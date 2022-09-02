adafd5765885842ba36a230b67e8ee555c072201
Yannick Bohrer <yannick.bohrer@collatum-it.de>
2022-09-02 13:52:47 +0200

    implemented submitPerson to post data into db

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  SERVICE_API_URL: "http://localhost:8081"
+  SERVICE_API_URL: "http://localhost:8082"
 };
 /*
  * For easier debugging in development mode, you can import the following file
