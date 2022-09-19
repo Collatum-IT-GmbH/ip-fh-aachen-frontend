1ff0da6b6f45fa2ae3436c2d143e66ecd4b0cab5
Yannick Bohrer <yannick.bohrer@collatum-it.de>
2022-09-19 15:19:11 +0200

    added routing

--- src/app/input-interface/input-interface.component.ts
+++ src/app/input-interface/input-interface.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {Person} from "../business/models/models";
 import {IpFhService} from "../business/services/ipFhService.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-input-interface',
   templateUrl: './input-interface.component.html',
-  styleUrls: ['./input-interface.component.css']
+  styleUrls: [
+    './input-interface.component.css',
+    '../shared/shared-styles.scss'
+  ]
 })
 export class InputInterfaceComponent implements OnInit {
   person = new Person();
 
   constructor(
+    private router: Router,
     private ipFhService: IpFhService
   ) {}
 
@@ -20,5 +25,9 @@ export class InputInterfaceComponent implements OnInit {
   submitPerson() {
     console.log(this.person);
     this.ipFhService.submitPerson(this.person).subscribe();
+  }
+
+  navigateTo(input: String) {
+    this.router.navigate(['/' + input]).then();
   }
 }
